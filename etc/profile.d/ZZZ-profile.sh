8cebb098b7097595ca2dc3bf4d302fa00c2199a3
Trevor Wencl <twencl@gmail.com>
2013-08-16 23:22:51 +0000

    Cool CLI color profile

---
+++ etc/profile.d/ZZZ-profile.sh
@@ -0,0 +1,7 @@
+# color profile
+export TERM=xterm-color
+export GREP_OPTIONS='--color=auto' GREP_COLOR='1;32'
+export CLICOLOR=1
+export LSCOLORS="exfxcxdxbxegedabagacad"
+PS1='\[\e[0;33m\]\u\[\e[0m\]@\[\e[0;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w\[\e[0m\]\$ '
+
